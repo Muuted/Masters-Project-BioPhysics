0b13911234193b5157361b4f44df7bf24b8b63b2
Muuted <adam.s.knudsen@gmail.com>
2025-11-14 18:47:08 +0100

    sdf

--- Surface model/import_matlab_data.py
+++ Surface model/import_matlab_data.py
@@ -1,3 +1,18 @@
 import numpy as np
 import pandas as pd
 
+
+
+data_path = "2D sim results\\" 
+data_names = ["tau_list_curved.dat","tau list curved.txt"]
+
+data = data_path + data_names[0]
+
+df_data= pd.read_csv(data)
+
+
+print(
+    np.shape(df_data)
+)
+
+print(df_data["Unnamed: 1"])#["sigma curved"][:])
