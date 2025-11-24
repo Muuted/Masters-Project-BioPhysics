79bb219625c8de4eddc96fa143e8d65caa8c181c
Muuted <adam.s.knudsen@gmail.com>
2025-11-24 15:58:43 +0100

    sf

--- Surface model/Two_D_constants.py
+++ Surface model/Two_D_constants.py
@@ -15,7 +15,7 @@ np.set_printoptions(legacy='1.25')
 
 def Two_D_paths(folder_names=""):
     """------ paths ---------"""
-    save_path =  "2D sim results\\" + "Data for thesis\\test multi\\"#Data simulation\\"#"Verification\\"
+    save_path =  "2D sim results\\" + "Data for thesis\\multi processor result\\"#Data simulation\\"#"Verification\\"
     if folder_names == "":
         save_path = save_path +  "c0=0 tau=0\\"
     else:
@@ -134,7 +134,7 @@ def Two_D_Constants_stationary_state(
     if N == "":
         N = 20#20 #60#20#80 #int(L/ds) # 99 + 1 # Number of chain links
     #m = 1e-6 # grams  :   Mass of each chain link
-    T = 1e-10#1e-7#1e-8 #5e-7 #1e-6 #3e-7# 0.3e-6# 20e-7 #10 #5.45#s  : total time simulated seconds
+    T = 1e-11#1e-7#1e-8 #5e-7 #1e-6 #3e-7# 0.3e-6# 20e-7 #10 #5.45#s  : total time simulated seconds
     dt = 1.25e-13#0.125e-12 #1e-11 #5e-11 #s time step. 
     sim_steps = int(T/dt) # : number of simulation steps
     L = 100.0 #1e-6 # micrometers  :  Total length of line
