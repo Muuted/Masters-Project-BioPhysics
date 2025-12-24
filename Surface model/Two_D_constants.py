26bf6f506a6c70af97f89655c5f7409a71908f2f
Muuted <adam.s.knudsen@gmail.com>
2025-12-24 02:02:54 +0100

    sdf

--- Surface model/Two_D_constants.py
+++ Surface model/Two_D_constants.py
@@ -135,8 +135,8 @@ def Two_D_Constants_stationary_state(
     if N == "":
         N = 20#20#20 #60#20#80 #int(L/ds) # 99 + 1 # Number of chain links
     #m = 1e-6 # grams  :   Mass of each chain link
-    T = 3e-9 #1e-7#1e-7#1e-7#1e-8 #5e-7 #1e-6 #3e-7# 0.3e-6# 20e-7 #10 #5.45#s  : total time simulated seconds
-    dt = 1.0e-14#0.125e-12 #1e-11 #5e-11 #s time step. 
+    T = 1e-7 #1e-7#1e-7#1e-7#1e-8 #5e-7 #1e-6 #3e-7# 0.3e-6# 20e-7 #10 #5.45#s  : total time simulated seconds
+    dt = 1.0e-13#0.125e-12 #1e-11 #5e-11 #s time step. 
     sim_steps = int(T/dt) # : number of simulation steps
     L = 100.0 #1e-6 # micrometers  :  Total length of line
     if ds == "":
